606828659c25662b40b605038455ccce25f0d636
Abner <abnercabrera28@gmail.com>
2021-03-24 18:52:32 -0400

    added routes to wrestlers controller

--- app/controllers/wrestler_controller.rb
+++ app/controllers/wrestler_controller.rb
@@ -1,3 +1,32 @@
 class WrestlersController < ApplicationController
 
+    get '/wrestlers' do
+        
+
+    end
+
+    get '/wrestlers/new' do
+
+    end
+
+    post '/wrestlers' do
+
+    end
+
+    get '/wrestlers/:id' do
+
+    end
+
+    get '/wrestlers/:id/edit' do
+
+    end
+
+    patch '/wrestlers/:id' do
+
+    end
+
+    delete '/wrestlers/:id' do
+
+    end
+
 end
