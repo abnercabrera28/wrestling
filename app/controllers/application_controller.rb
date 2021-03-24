51c8c96039316e10f4b68dae6c1f556a6f147942
Abner <abnercabrera28@gmail.com>
2021-03-24 14:31:39 -0400

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,22 @@ class ApplicationController < Sinatra::Base
         set :session_secret, ENV['SESSION_SECRET']
     end
 
+    helpers do 
+        def current_user
+            @current_user ||= User.find_by_id(session["user_id"])
+        end
+
+        def logged_in?
+            !!current_user
+        end
+
+        def redirect_if_not_logged_in
+            redirect "/login" if !logged_in?
+        end
+
+        def redirect_if_logged_in
+            redirect "/wrestlers" if logged_in?
+        end
+    end
+
 end
