39ee905907f2722ec3257753672c807b597a40f9
Abner <abnercabrera28@gmail.com>
2021-03-24 15:30:30 -0400

    added code to login routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,19 @@
 class SessionsController < ApplicationController
 
     get '/login' do
-
+        redirect_if_logged_in
+        erb :"/sessions/new"
     end
 
     post '/login' do
-        
+        user = User.find_by(username: params["user"]["username"])
+
+        if user && user.authenticate(params["user"]["password"])
+            session["user_id"] = user.id
+            redirect "/wrestlers"
+        else
+            redirect "/login"
+        end
     end
+    
 end
